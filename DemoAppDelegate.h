d72ae712a007fa5e2d4dda18bc9d3bd63fdd8a57
ataraxia2012 <ataraxia2012@gmail.com>
2012-12-31 22:57:39 +0800

    new test update
    new test update

---
+++ DemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DemoAppDelegate.h
+//  CocoaDemo
+//
+//  Created by 霍刚 on 12-12-31.
+//  Copyright (c) 2012年 ataraxia2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CocoaDemo
+//
+//  Created by 霍刚 on 12-12-31.
+//  Copyright (c) 2012年 ataraxia2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DemoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DemoAppDelegate class]));
+    }
+}
